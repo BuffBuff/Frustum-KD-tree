15dc91b8d70539041ade7ebcabdc71845e901d59
Kraiper <martin_svart@hotmail.com>
2015-11-09 14:04:15 +0100

    Fixed shader settings

--- BaseWindow(with raytracer)/BaseWindow/sortListPass.hlsl
+++ BaseWindow(with raytracer)/BaseWindow/sortListPass.hlsl
@@ -1,11 +1,11 @@
-
+#include "Collisions.fx"
 
 ConsumeStructuredBuffer<int2> AppendIndiceBuffer : register(u0);
 
 RWStructuredBuffer<int2> IndiceBuffer : register(u1);
 
 [numthreads(CORETHREADSWIDTH, CORETHREADSHEIGHT, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+void main(uint3 threadID : SV_DispatchThreadID)
 {
 
 
